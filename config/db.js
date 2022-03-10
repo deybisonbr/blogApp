479cd9a8b0dae077498b887b0978224c4ced2066
Daybison Braga <95456341+deybisonbr@users.noreply.github.com>
2022-03-10 15:15:44 -0300

    config

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI: 'mongodb+srv://daybisonbraga:dede654321@blogapp.r8afi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'}
+    module.exports = {mongoURI: 'Link Database MongoDB'}
 }else{
     module.exports = {mongoURI: 'mongodb://localhost/blogapp'}
 }
